8a3c337598a295e1d082baeb595f52da57331572
Ibrahim Aziz <ibraziz21@gmail.com>
2023-01-19 11:58:59 +0300

    Create NavBar.jsx

---
+++ portfolio/src/components/navbar/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import './NavBar.css'
+const NavBar = ()=>{
+return (
+       <div className="n-wrapper">
+        <div className="n-left">
+            <div className="n-name">Ibrahim</div>
+            <span>toggle</span>
+        </div>
+        <div className="n-right">
+           <div className="n-list">
+            <ul>
+                <li>Home</li>
+                <li>Services</li>
+                <li>Experience</li>
+                <li>Portfolio</li>
+                <li>Testimonials</li>
+            </ul>
+        </div>
+        <button className="button">Contact Us</button>
+        </div>
+        </div>
+        
+    
+)
+
+}
+export default NavBar
