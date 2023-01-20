4ee452006857430f0d71af606d9033352fa822a2
Ibrahim Aziz <ibraziz21@gmail.com>
2023-01-20 09:55:46 +0300

    Update App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import NavBar from './components/navbar/NavBar';
+import Intro from './components/intro/Intro';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <NavBar/>
+     <Intro/>
      </div>
   );
 }
